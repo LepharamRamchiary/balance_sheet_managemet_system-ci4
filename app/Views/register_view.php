03ccae954d753be7f00189481db2b79498e964ed
LepharamRamchiary <lepharamchiary@gmail.com>
2024-11-14 07:46:38 +0530

    db connection

--- app/Views/register_view.php
+++ app/Views/register_view.php
@@ -11,34 +11,29 @@
 
                     <!-- Form -->
                     <?php echo form_open(); ?>
-                        <div class="mb-3">
-                            <label class="form-label" for="username">Username</label>
-                            <input type="text" class="form-control" name="username" id="username" value="<?= set_value('username') ?>" required>
-                        </div>
-                        
-                        <div class="mb-3">
-                            <label class="form-label" for="email">Email</label>
-                            <input type="email" class="form-control" name="email" id="email" value="<?= set_value('email') ?>" required>
-                        </div>
-                        
-                        <div class="mb-3">
-                            <label class="form-label" for="phone">Phone No</label>
-                            <input type="text" class="form-control" name="phone" id="phone" value="<?= set_value('phone') ?>" required>
-                        </div>
-                        
-                        <div class="mb-3">
-                            <label class="form-label" for="password">Password</label>
-                            <input type="password" class="form-control" name="password" id="password" required>
-                        </div>
-                        
-                        <div class="mb-3">
-                            <label class="form-label" for="conf_password">Confirm Password</label>
-                            <input type="password" class="form-control" name="conf_password" id="conf_password" required>
-                        </div>
+                    <div class="mb-3">
+                        <label class="form-label" for="username">Username</label>
+                        <input type="text" class="form-control" name="username" id="username" value="<?= set_value('username') ?>" required>
+                    </div>
 
-                        <div class="d-grid gap-2">
-                            <input type="submit" class="btn btn-primary btn-block" value="Register" name="register">
-                        </div>
+                    <div class="mb-3">
+                        <label class="form-label" for="email">Email</label>
+                        <input type="email" class="form-control" name="email" id="email" value="<?= set_value('email') ?>" required>
+                    </div>
+
+                    <div class="mb-3">
+                        <label class="form-label" for="password">Password</label>
+                        <input type="password" class="form-control" name="password" id="password" required>
+                    </div>
+
+                    <div class="mb-3">
+                        <label class="form-label" for="conf_password">Confirm Password</label>
+                        <input type="password" class="form-control" name="conf_password" id="conf_password" required>
+                    </div>
+
+                    <div class="d-grid gap-2">
+                        <input type="submit" class="btn btn-primary btn-block" value="Register" name="register">
+                    </div>
                     <?php echo form_close(); ?>
 
                     <p class="text-center mt-3">Already have an account? <a href="<?= base_url() ?>loginboth">Login here</a></p>
